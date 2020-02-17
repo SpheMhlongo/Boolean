8d6210f7bc1fda110fbb9ce01aec19bea362ddba
SpheMhlongo <61015938+SpheMhlongo@users.noreply.github.com>
2020-02-17 12:23:47 +0200

    added a method to construct basic truth table

--- Boolean.py
+++ Boolean.py
@@ -6,10 +6,16 @@
 # P equivalence Q and !R or Z
 
 import re
+import itertools
 
 def get_variables(expression):
-    return re.findall(r"\b(\w)\b", expression)
+    return set(re.findall(r"\b(\w)\b", expression))
 
-print(get_variables("P equivalence Q and !R or Z"))
+def construct_variable_truth_combinations(variables):
+    n_variables = len(variables)
+    return set(itertools.combinations([1, 0] * n_variables, n_variables))
+    
+
+print(construct_variable_truth_combinations(get_variables("P equivalence Q and !R or Z")))
 print(get_variables("(!P implication !Q) and Q implication P"))
 print(get_variables("!P and !Q or R"))
